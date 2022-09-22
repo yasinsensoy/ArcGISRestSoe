08672ccdbcafb135da994b3bfd7ed4e30d5e098b
Yasin Şensoy <ysensoy@konumsal.com.tr>
2022-09-22 13:35:21 +0300

    Add project files.

---
+++ OrbisKroki/OrbisKroki.cs
@@ -0,0 +1,149 @@
+﻿// Copyright 2018 ESRI
+// 
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+// 
+// You may freely redistribute and use this sample code, with or
+// without modification, provided you include the original copyright
+// notice and use restrictions.
+// 
+// See the use restrictions at <your Enterprise SDK install location>/userestrictions.txt.
+// 
+
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Server;
+using ESRI.Server.SOESupport;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+//This is REST SOE template of Enterprise SDK
+
+//TODO: sign the project (project properties > signing tab > sign the assembly)
+//      this is strongly suggested if the dll will be registered using regasm.exe <your>.dll /codebase
+
+
+namespace OrbisKroki
+{
+    [ComVisible(true)]
+    [Guid("8cee4b75-9ba9-40f1-b54a-a35a68a1b67a")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ServerObjectExtension("MapServer",
+        AllCapabilities = "",
+        DefaultCapabilities = "",
+        Description = "",
+        DisplayName = "OrbisKroki",
+        Properties = "",
+        SupportsREST = true,
+        SupportsSOAP = false,
+        SupportsSharedInstances = false)]
+    public class OrbisKroki : IServerObjectExtension, IObjectConstruct, IRESTRequestHandler
+    {
+        private string soe_name;
+
+        private IPropertySet configProps;
+        private IServerObjectHelper serverObjectHelper;
+        private ServerLogger logger;
+        private IRESTRequestHandler reqHandler;
+
+        public OrbisKroki()
+        {
+            soe_name = this.GetType().Name;
+            logger = new ServerLogger();
+            reqHandler = new SoeRestImpl(soe_name, CreateRestSchema()) as IRESTRequestHandler;
+        }
+
+        #region IServerObjectExtension Members
+
+        public void Init(IServerObjectHelper pSOH)
+        {
+            serverObjectHelper = pSOH;
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        #endregion
+
+        #region IObjectConstruct Members
+
+        public void Construct(IPropertySet props)
+        {
+            configProps = props;
+        }
+
+        #endregion
+
+        #region IRESTRequestHandler Members
+
+        public string GetSchema()
+        {
+            return reqHandler.GetSchema();
+        }
+
+        public byte[] HandleRESTRequest(string Capabilities, string resourceName, string operationName, string operationInput, string outputFormat, string requestProperties, out string responseProperties)
+        {
+            return reqHandler.HandleRESTRequest(Capabilities, resourceName, operationName, operationInput, outputFormat, requestProperties, out responseProperties);
+        }
+
+        #endregion
+
+        private RestResource CreateRestSchema()
+        {
+            RestResource rootRes = new RestResource(soe_name, false, RootResHandler);
+
+            RestOperation sampleOper = new RestOperation("sampleOperation",
+                                                      new string[] { "parm1", "parm2" },
+                                                      new string[] { "json" },
+                                                      SampleOperHandler);
+
+            rootRes.operations.Add(sampleOper);
+
+            return rootRes;
+        }
+
+        private byte[] RootResHandler(NameValueCollection boundVariables, string outputFormat, string requestProperties, out string responseProperties)
+        {
+            responseProperties = null;
+
+            JsonObject result = new JsonObject();
+            result.AddString("hello", "world");
+
+            return Encoding.UTF8.GetBytes(result.ToJson());
+        }
+
+        private byte[] SampleOperHandler(NameValueCollection boundVariables,
+                                                  JsonObject operationInput,
+                                                      string outputFormat,
+                                                      string requestProperties,
+                                                  out string responseProperties)
+        {
+            responseProperties = null;
+
+            string parm1Value;
+            bool found = operationInput.TryGetString("parm1", out parm1Value);
+            if (!found || string.IsNullOrEmpty(parm1Value))
+                throw new ArgumentNullException("parm1");
+
+            string parm2Value;
+            found = operationInput.TryGetString("parm2", out parm2Value);
+            if (!found || string.IsNullOrEmpty(parm2Value))
+                throw new ArgumentNullException("parm2");
+
+            JsonObject result = new JsonObject();
+            result.AddString("parm1", parm1Value);
+            result.AddString("parm2", parm2Value);
+
+            return Encoding.UTF8.GetBytes(result.ToJson());
+        }
+
+
+    }
+}
---
+++ OrbisKroki/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OrbisKroki")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OrbisKroki")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a88cdbbd-171a-494f-8878-ce5bee355615")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+[assembly: ESRI.Server.SOESupport.AddInPackage("OrbisKroki", "89158157-359b-4417-9157-83c7424dfc89",
+    Author = "izambakci",
+    Company = "",
+    Date = "9/22/2022 1:33:46 PM",
+    Description = "",
+    TargetProduct = "Server",
+    TargetVersion = "10.9",
+    Version = "1.0")]
